1f6dd49e26b5783e85958ea0f860ce9bcf84e59a
euhansarkar <euhansarkar@gmail.com>
2024-09-24 03:25:00 +0600

    Added: new INITTTT.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes";
 
-const App = () => {
+function App() {
   return (
-    <div className='font-bold text-7xl'>
-      hello world
-    </div>
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
   );
-};
+}
 
 export default App;
